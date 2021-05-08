21b132b2076adb9475fd4090cb11f057f713c609
Matheus Gabrie <matheuscoding@gmail.com>
2021-05-08 13:05:00 -0300

    Retirando a necessidade do detector de faces do OpenCV
    Co-authored-by: Ana <ancarolcs@gmail.com>
    Co-authored-by: Lucas <lucascandradex14@gmail.com>

--- dect_faces.py
+++ dect_faces.py
@@ -7,8 +7,6 @@ from keras.preprocessing import image
 
 cam = cv.VideoCapture(0)
 
-classifier = cv.CascadeClassifier(f"model.xml")
-
 cnn = keras.models.load_model('mask_detector_model')
 
 while True:
@@ -22,25 +20,17 @@ while True:
 
     cv.imwrite('frame.png',frame)
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-    faces = classifier.detectMultiScale(gray)
-
-    for x,y,w,h in faces:
-        gray_face = gray[y:y+h, x:x+w]
-
-        if gray_face.shape[0] >= 200 and gray_face.shape[1] >= 200:
-            test_image = image.load_img('frame.png', target_size = (64, 64))
-            test_image = image.img_to_array(test_image)
-            test_image = np.expand_dims(test_image, axis = 0)
-            result = cnn.predict(test_image)
-            if result[0][0] == 1:
-                prediction = 'COM mascara'
-                cv.rectangle(frame,(x,y),(x+w,x+h),(0,255,0), 3)
-                cv.putText(frame,prediction,(x - 20,y + h + 50), cv.FONT_HERSHEY_COMPLEX,0.5, (243,255,0), 2, cv.LINE_AA)
-            else:
-                prediction = 'SEM mascara'
-                cv.rectangle(frame,(x,y),(x+w,x+h),(0,40,255), 3)
-                cv.putText(frame,prediction,(x - 20,y + h + 50), cv.FONT_HERSHEY_COMPLEX,0.5, (243,255,0), 2, cv.LINE_AA)
+    test_image = image.load_img('frame.png', target_size = (64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = cnn.predict(test_image)
+    if result[0][0] == 1:
+        prediction = 'COM mascara'
+        cv.rectangle(frame,(150,15),(500,85),(255,255,255), -1)
+        cv.putText(frame,prediction,(0 + 200,0 + 0 + 50), cv.FONT_HERSHEY_COMPLEX,1, (56,142,72), 2, cv.LINE_AA)
+    else:
+        prediction = 'SEM mascara'
+        cv.rectangle(frame,(150,15),(500,85),(255,255,255), -1)
+        cv.putText(frame,prediction,(0 + 200,0 + 0 + 50), cv.FONT_HERSHEY_COMPLEX,1,(0,40,255) , 2, cv.LINE_AA)
 
     cv.imshow("Screen",frame)
