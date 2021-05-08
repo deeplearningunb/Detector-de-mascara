d74ea63da632314326b521eb4167538d4863531b
Matheus Gabrie <matheuscoding@gmail.com>
2021-05-08 09:35:55 -0300

    Adicionando integracao entre o modelo de deteccao de mascaras e o de detectar faces

--- dect_faces.py
+++ dect_faces.py
@@ -1,9 +1,15 @@
 import cv2 as cv
 import os
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow import keras
+import numpy as np
+from keras.preprocessing import image
 
 cam = cv.VideoCapture(0)
 
 classifier = cv.CascadeClassifier(f"model.xml")
+
+cnn = keras.models.load_model('mask_detector_model')
 
 while True:
     status, frame = cam.read()
@@ -14,6 +20,8 @@ while True:
     if cv.waitKey(1) & 0xff == ord('q'):
         break
 
+    cv.imwrite('frame.png',frame)
+
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     faces = classifier.detectMultiScale(gray)
@@ -22,9 +30,17 @@ while True:
         gray_face = gray[y:y+h, x:x+w]
 
         if gray_face.shape[0] >= 200 and gray_face.shape[1] >= 200:
-
-            cv.rectangle(frame,(x,y),(x+w,x+h),(0,255,0), 3)
-
-            cv.putText(frame,"Um rosto",(x - 20,y + h + 50), cv.FONT_HERSHEY_COMPLEX,0.5, (243,255,0), 2, cv.LINE_AA)
+            test_image = image.load_img('frame.png', target_size = (64, 64))
+            test_image = image.img_to_array(test_image)
+            test_image = np.expand_dims(test_image, axis = 0)
+            result = cnn.predict(test_image)
+            if result[0][0] == 1:
+                prediction = 'COM mascara'
+                cv.rectangle(frame,(x,y),(x+w,x+h),(0,255,0), 3)
+                cv.putText(frame,prediction,(x - 20,y + h + 50), cv.FONT_HERSHEY_COMPLEX,0.5, (243,255,0), 2, cv.LINE_AA)
+            else:
+                prediction = 'SEM mascara'
+                cv.rectangle(frame,(x,y),(x+w,x+h),(0,40,255), 3)
+                cv.putText(frame,prediction,(x - 20,y + h + 50), cv.FONT_HERSHEY_COMPLEX,0.5, (243,255,0), 2, cv.LINE_AA)
 
     cv.imshow("Screen",frame)
