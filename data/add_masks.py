1174822f31fa3e4592be3d87946ecda4c391059e
Matheus Gabrie <matheuscoding@gmail.com>
2021-05-08 11:17:34 -0300

    Adicionando no script de geracao das imagens com mascaras a parte de gerar com as diferentes mascaras

--- data/add_masks.py
+++ data/add_masks.py
@@ -1,6 +1,10 @@
 import os
+import random
 from PIL import Image
+
 parent_dir = 'nomask/'
+
+masks = ['mask1.png', 'mask2.png', 'mask3.png', 'mask4.png']
 
 for subdir, dirs, files in os.walk(parent_dir):
     for count, file in enumerate(files):
@@ -8,11 +12,7 @@ for subdir, dirs, files in os.walk(parent_dir):
         img = Image.open(image_path)
 
         img2 = img.copy()
-        
-        mask = Image.open("mask2.png")
+        mask = Image.open(random.choice(masks))
 
         img.paste(mask,(0,0),mask)
-        
         img.save(f"withmask/withMask{count}.png")
-        
-        
